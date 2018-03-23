b1d216ce3ff1f3307e2b6bce6086bf5f978f6e0f
AntoineSb <antoine.sb@orange.fr>
2018-03-23 16:33:34 +0000

    réorganisation

--- src/auctioneer/ServerGUI.java
+++ src/auctioneer/ServerGUI.java
@@ -1,25 +1,45 @@
 package auctioneer;
 
+import common.Utility;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ServerGUI extends Application implements Runnable {
-	private String[] args;
-	
-	public ServerGUI(String[] args) {
-		this.args = args;
-	}
-
-	@Override
-	public void run() {
-		launch(args);
-		
-	}
-
-	@Override
-	public void start(Stage args) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
+	/* attributes */
+		private String[] args;
+	/* members */
+		// constrcutor
+			public ServerGUI(String[] args) {
+				this.args = args;
+			}
+		// thread
+			@Override
+			public void run() {
+				launch(args);
+				
+			}
+		// javafx
+			@Override
+			public void start(Stage primaryStage) throws Exception {
+				primaryStage.setTitle("auctioneer");
+				Button btn = new Button();
+				btn.setText("Invoke Satan");
+				btn.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						println("Gort ! Klaatu barada nikto !");
+					}
+				});
+				StackPane root = new StackPane();
+				root.getChildren().add(btn);
+				primaryStage.setScene(new Scene(root, 300, 250));
+				primaryStage.show();
+			}
+		// display
+			private static void println(String data) { Utility.println("[SERVER_UI]> " + data); }
 }
