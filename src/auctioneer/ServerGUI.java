d26110052e76e8c51c327445b83e6072bdd465ad
AntoineSb <antoine.sb@orange.fr>
2018-03-23 21:38:29 +0000

    Début de l'implémentation des composants graphiques

--- src/auctioneer/ServerGUI.java
+++ src/auctioneer/ServerGUI.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,14 +16,10 @@ public class ServerGUI extends Application implements Runnable {
 	/* members */
 		// constructor
 			public ServerGUI() {}
-			public ServerGUI(String[] args) {
-				this.args = args;
-			}
+			public ServerGUI(String[] args) { this.args = args; }
 		// thread
 			@Override
-			public void run() {
-				launch(args);
-			}
+			public void run() { launch(args); }
 		// javafx
 			@Override
 			public void start(Stage primaryStage) throws Exception {
@@ -39,6 +36,24 @@ public class ServerGUI extends Application implements Runnable {
 				root.getChildren().add(btn);
 				primaryStage.setScene(new Scene(root, 300, 250));
 				primaryStage.show();
+			}
+		// javafx components
+			private TextField currentTime() {
+				TextField currentTimeTextField = new TextField ();
+				currentTimeTextField.setText("Label");
+				return currentTimeTextField;
+			}
+			// current time (textfield)
+			// time remaining (textfield)
+			// start/stop (button)
+			// create product (editable textfield)
+			// send product info (button)
+			// send time remaining (button)
+			// highest bid received (textfield)
+			// highest bid client IP (textfield)
+			// console (scrollable textarea)
+			private TextField createTextFiel() {
+				return new TextField();
 			}
 		// display
 			private static void println(String data) { Utility.println("[SERVER_UI]> " + data); }
