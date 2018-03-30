860584c0d16d4139d7469d61660516aa36d3fbb8
AntoineSb <antoine.sb@orange.fr>
2018-03-30 17:14:42 +0100

    trucs

--- src/customer/ClientGUI.java
+++ src/customer/ClientGUI.java
@@ -1,7 +1,10 @@
 package customer;
 
+import common.Utility;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ClientGUI extends Application implements Runnable {
@@ -22,6 +25,44 @@ public class ClientGUI extends Application implements Runnable {
 			public void start(Stage primaryStage) throws Exception {
 				primaryStage.setTitle("client");
 				root = new BorderPane();
-		
+				// TOP
+					// the time left (in seconds) to the end-of-bidding deadline
+				// LEFT
+					// text field to allow the client to enter a new bid
+					// Button to submit a new bid 
+				// CENTER
+					// product info
+				// RIGHT
+					// value of the highest bid received to date
+				// BOTTOM
+					// disconnect
+				primaryStage.setScene(new Scene(root, 1200, 600));
+				primaryStage.show();
+			}
+			@Override
+			public void stop() {
+				println("Closing user interface...");
+				try {
+					super.stop();
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+				println("User interface closed");
+			}
+		// client-side events
+			public static void printConsole(String data) {
+				try {
+					Text console = (Text)scrollableConsole.getContent();
+					console.setText(console.getText() + '[' + Utility.getStringTime() + "]" + data + '\n');
+				}
+				catch(NullPointerException e) {
+					Utility.println("[CLIENT]> Graphic console not available");
+				}
+			}
+		// display
+			private static void println(String data) {
+				Utility.println("[CLIENT]> " + data);
+				printConsole("[CLIENT]> " + data);
 			}
 }
